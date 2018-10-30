84dc93831988123ffcf53fbe0744b4fa1661ebec
keithpoitras123@outlook.com <ARpower123>
2018-10-30 11:33:40 -0400

    new changes. course 01

--- GitFile.py
+++ GitFile.py
@@ -1,1 +1,18 @@
-0
+print("X", "Y")
+
+print("g")
+
+t = "test" + \
+    "asf"
+
+print(t)
+
+fist_name = "Brandan"
+last_name = "Wood"
+number = 42
+
+result = fist_name + " " + last_name + " " + "and my favorite " + \
+         "number is " + str(number)
+
+print(result)
+
