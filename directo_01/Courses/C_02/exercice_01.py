b3bb97eba691dbe8594a497b814d424b91eb5f89
keithpoitras123@outlook.com <ARpower123>
2018-11-13 11:52:59 -0500

    class2 exercise 1

---
+++ directo_01/Courses/C_02/exercice_01.py
@@ -0,0 +1,45 @@
+x = int(input("\n" + "Enter the year you started to work : "))
+y = int(input("\n" + "Enter the current year : "))
+
+z = str(y - x)
+
+print("You have been working for " + z + " years".format(z))
+
+try:
+    print(5/0)
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+
+# 1A
+c = 1
+d = "A"
+print(Str(c) + d)
+
+# ax
+
+c = "a"
+d = "x"
+print(c + d)
+
+
+a = str(input("\n" + "Enter your first answer: "))
+
+b = str(input("\n" + "Enter your second answer: "))
+
+print(a + b)
+
+# BREAK
+for b in range(10)
+    if b == 3:
+        break
+    print(b)
+
+# CONTINUE
+for b in range(10)
+    if b == 3:
+        continue
+    print(b)
+
+
+
+
